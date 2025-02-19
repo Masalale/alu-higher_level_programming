0d147a5b77f861d2af697afcfdd015f971e0e1ad
Masalale <clarenchomba59@gmail.com>
2025-02-19 14:07:19 +0200

    Enhance argument handling in 2-args.py to provide detailed output based on the number of arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,6 +3,14 @@ import sys
 
 if __name__ == "__main__":
     args = sys.argv[1:]
-    print("Number of arguments:", len(args))
-    print("Arguments:", args)
+    length = len(args)
     
+    if length == 0:
+        print("0 arguments.")
+    elif length == 1:
+        print("1 argument:")
+        print("1: {}".format(args[0]))
+    else:
+        print("{} arguments:".format(length))
+        for i in range(length):
+            print("{}: {}".format(i + 1, args[i]))
