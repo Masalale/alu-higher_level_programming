367562ab7659569b3e4a9ae83d8a0468ed58dfe5
Masalale <clarenchomba59@gmail.com>
2025-02-19 13:58:59 +0200

    Refactor addition script to use variables for clarity in output

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,6 +3,7 @@ def add(a, b):
     return a + b
 
 if __name__ == "__main__":
-    result = add(1, 2)
-    print(result)
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
     
