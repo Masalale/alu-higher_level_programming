82318f3048b3ed9671220bbaf4df164b04e01ae2
Masalale <clarenchomba59@gmail.com>
2025-02-27 14:40:58 +0200

    Fix docstring formatting in is_kind_of_class and inherits_from functions for improved readability

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,7 +3,8 @@
 
 
 def inherits_from(obj, a_class):
-    """Check if object is an instance of a class that inherited from specified class
+    """Check if object is an instance of a class that inherited from
+    specified class
 
     Args:
         obj: The object to check
