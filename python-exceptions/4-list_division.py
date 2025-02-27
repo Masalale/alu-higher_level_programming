8df1f74ccca07483cf9b51b447b3b694dcb14af4
Masalale <clarenchomba59@gmail.com>
2025-02-27 11:26:32 +0200

    Add initial implementations of exception handling functions and update README documentation

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(list_length):
+        result = 0
+        try:
+            if i >= len(my_list_1) or i >= len(my_list_2):
+                raise IndexError
+            
+            if not (isinstance(my_list_1[i], (int, float)) and 
+                   isinstance(my_list_2[i], (int, float))):
+                raise TypeError
+            
+            result = my_list_1[i] / my_list_2[i]
+            
+        except ZeroDivisionError:
+            print("division by 0")
+        except TypeError:
+            print("wrong type")
+        except IndexError:
+            print("out of range")
+        finally:
+            new_list.append(result)
+    return new_list
