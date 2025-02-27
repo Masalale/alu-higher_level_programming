2cfdc86fb1de1393873637aaadaa5a40c378b671
Masalale <clarenchomba59@gmail.com>
2025-02-27 11:29:44 +0200

    Fix newline issues and improve formatting in safe_print_division.py and list_division.py

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -6,13 +6,13 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             if i >= len(my_list_1) or i >= len(my_list_2):
                 raise IndexError
-            
-            if not (isinstance(my_list_1[i], (int, float)) and 
-                   isinstance(my_list_2[i], (int, float))):
+
+            if not (isinstance(my_list_1[i], (int, float)) and
+                    isinstance(my_list_2[i], (int, float))):
                 raise TypeError
-            
+
             result = my_list_1[i] / my_list_2[i]
-            
+
         except ZeroDivisionError:
             print("division by 0")
         except TypeError:
