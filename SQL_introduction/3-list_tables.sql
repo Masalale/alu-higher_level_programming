6fe36d067bf6f8112195fc75c7f160f22d111dd3
Masalale <clarenchomba59@gmail.com>
2025-03-13 18:16:09 +0200

    Fix comment typo and add SHOW TABLES command in list_tables.sql

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,2 +1,2 @@
--- cript that lists all the tables of a database in your MySQL.
-mysql -u username -p database_name < 3-list_tables.sql
+-- Script that lists all the tables of a database in your MySQL server
+SHOW TABLES;
