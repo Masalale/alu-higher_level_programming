1ae83525e60ee5c38a27f0a8b954f7895e5f5a60
Masalale <clarenchomba59@gmail.com>
2025-03-13 19:02:08 +0200

    Refactor privileges script to focus on listing grants for users user_0d_1 and user_0d_2

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,11 +1,4 @@
--- Script that creates MySQL users user_0d_1 and user_0d_2 with specific privileges and lists their privileges
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
-
+-- Script that lists all privileges of users user_0d_1 and user_0d_2
 FLUSH PRIVILEGES;
-
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
