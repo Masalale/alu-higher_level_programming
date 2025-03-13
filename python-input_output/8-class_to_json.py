186db55f30cf8701b9d25f6bdd628446b1e9447d
Masalale <clarenchomba59@gmail.com>
2025-03-13 14:44:13 +0200

    Refactor docstring in class_to_json function to improve clarity

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -7,10 +7,8 @@ Function to convert class to JSON serializable dictionary
 def class_to_json(obj):
     """
     Returns dictionary description of an object for JSON serialization
-    
     Args:
         obj: instance of a class with serializable attributes
-    
     Returns:
         Dictionary representation of the object
     """
