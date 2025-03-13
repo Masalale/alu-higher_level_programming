c77401f0880ba541b7057208f9a374c9d10d3b3e
Masalale <clarenchomba59@gmail.com>
2025-03-13 18:58:49 +0200

    Update privileges for user_0d_2 to grant access on user_2_db database

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -3,7 +3,7 @@ CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT, INSERT ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
 
 FLUSH PRIVILEGES;
 
