f79ccd1cfe1b1879e1d759ea7b24e21d803263a1
Masalale <clarenchomba59@gmail.com>
2025-03-13 13:45:41 +0200

    Add docstring to read_file function for improved documentation

--- python_input_output/0-read_file.py
+++ python_input_output/0-read_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+Function to read a text file and print its contents
+"""
+
+
 def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout
+    """
     with open(filename, encoding='utf-8') as file:
         print(file.read(), end="")
-
