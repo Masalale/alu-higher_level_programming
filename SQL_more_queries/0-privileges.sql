359956065aad41cba6b34480a2266fa51e310662
Masalale <clarenchomba59@gmail.com>
2025-03-13 18:52:47 +0200

    Add script to create MySQL users with specific privileges and list their grants

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,12 @@
--- Script that lists all privileges of users user_0d_1 and user_0d_2
+-- Script that creates MySQL users user_0d_1 and user_0d_2 with specific privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
+
 FLUSH PRIVILEGES;
+
+-- Script that lists all privileges of users user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
