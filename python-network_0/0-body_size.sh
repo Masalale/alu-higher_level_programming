2854c790e068b1cd557d4c6f834ea603296c4f0c
Masalale <clarenchomba59@gmail.com>
2025-03-19 22:30:56 +0200

    Remove URL validation from body size script to simplify input handling

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 # Script that displays the size of the response body in bytes
-
-# Prepend "http://" if the URL doesn't start with http or https
-if [[ $1 != http* ]]; then
-  url="http://$1"
-else
-  url="$1"
-fi
-
-curl -s "$url" | wc -c
+curl -s "$1" | wc -c
