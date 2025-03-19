ded754796373263f1913931ab61ea5773705269b
Masalale <clarenchomba59@gmail.com>
2025-03-19 22:40:27 +0200

    Update body size script to display content length from response headers

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that displays the size of the response body in bytes
-curl -s "$1" -o /dev/null -w '%{size_download}\n'
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
