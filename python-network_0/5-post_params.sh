3a6c87c3c908663db3ccf970e1c9276b7147ba0f
Masalale <clarenchomba59@gmail.com>
2025-03-19 23:01:57 +0200

    Add custom header to POST request in form submission script

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Submits form data using POST with email and subject parameters
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+# Submits form data using POST with email and subject parameters and custom header
+curl -s -X POST -H "X-HolbertonSchool-User-Id: 98" -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
