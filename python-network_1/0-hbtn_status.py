d76620106204229ea2f7ed7854d66d2acba3f5b6
Masalale <clarenchomba59@gmail.com>
2025-03-19 23:24:40 +0200

    Remove command-line URL input and set default status URL in script

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """
-Python script that fetches a status URL
+Python script that fetches https://alu-intranet.hbtn.io/status
 """
 import urllib.request
-import sys
 
 
 if __name__ == "__main__":
-    # Use provided URL or default to the original URL
-    url = sys.argv[1] if len(sys.argv) > 1 else "https://alu-intranet.hbtn.io/status"
+    url = "https://alu-intranet.hbtn.io/status"
     
     with urllib.request.urlopen(url) as response:
         body = response.read()
@@ -16,3 +14,4 @@ if __name__ == "__main__":
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode('utf-8')))
+        
