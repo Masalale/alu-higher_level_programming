3a8007f3fe34d7dcdd103656fee466b2d5507d70
Masalale <clarenchomba59@gmail.com>
2025-03-19 22:29:19 +0200

    Add URL validation to body size script by prepending "http://" if missing

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 # Script that displays the size of the response body in bytes
-curl -s "$1" | wc -c
+
+# Prepend "http://" if the URL doesn't start with http or https
+if [[ $1 != http* ]]; then
+  url="http://$1"
+else
+  url="$1"
+fi
+
+curl -s "$url" | wc -c
