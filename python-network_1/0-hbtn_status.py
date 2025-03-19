2b9743670004862e372a08cfb2d70d98cdace5bc
Masalale <clarenchomba59@gmail.com>
2025-03-19 23:37:01 +0200

    Remove unnecessary blank lines in the status script

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,8 +5,6 @@ Python script that fetches https://intranet.hbtn.io/status
 import urllib.request
 
 
-
-
 if __name__ == '__main__':
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         content = response.read()
