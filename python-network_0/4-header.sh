5e41cb561ca34f307c3d1b3b2b698143088e9298
Masalale <clarenchomba59@gmail.com>
2025-03-19 23:06:16 +0200

    Update GET request to follow redirects with -L option in header script

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a GET request with a custom user ID header to identify the client
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -sL -H "X-HolbertonSchool-User-Id: 98" "$1"
