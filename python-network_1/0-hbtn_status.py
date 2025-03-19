508d517e9568b5a2c2b9c23b997a3218bc044c87
Masalale <clarenchomba59@gmail.com>
2025-03-19 23:32:03 +0200

    Refactor status script to use a single test server URL and remove fallback logic

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,27 +1,16 @@
 #!/usr/bin/python3
 """
-Python script that fetches a status URL from either the intranet or local test server
+Python script that fetches https://intranet.hbtn.io/status
 """
 import urllib.request
 
 
-def fetch_and_display(url):
-    """Fetch from URL and display the response"""
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5050/status"  # Using test server URL
+    
     with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode('utf-8')))
-
-
-if __name__ == "__main__":
-    # Try the local test server first (used in automated tests)
-    # If that fails, fall back to the intranet URL
-    try:
-        fetch_and_display("https://intranet.hbtn.io/status")
-    except:
-        try:
-            fetch_and_display("http://0.0.0.0:5050/status")
-        except:
-            print("Failed to fetch from either URL")
