e5ef501df6363e4936862910e3ee32f1e3a958b4
Masalale <clarenchomba59@gmail.com>
2025-03-19 22:59:06 +0200

    Update POST request to include Content-Type header for form data submission

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Submits form data using POST with email and subject parameters
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
