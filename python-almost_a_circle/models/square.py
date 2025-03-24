d20a639ed20961eb2dccec6ffeb3168986e6e10b
Masalale <clarenchomba59@gmail.com>
2025-03-24 21:37:27 +0200

    Add Square class that inherits from Rectangle for square representation

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Square module - implements Square class that inherits from Rectangle
+"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class for representing squares
+
+    Inherits from Rectangle since a square is a special case
+    of a rectangle where width equals height.
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new Square instance
+
+        Args:
+            size: Size of the square (both width and height)
+            x: X position of the square
+            y: Y position of the square
+            id: Identifier for this square
+        """
+        # Call the parent class constructor with id, x, y, width and height
+        # For a square, width and height are both equal to size
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Return a string representation of the Square
+
+        Returns:
+            Formatted string with square dimensions and position
+        """
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
