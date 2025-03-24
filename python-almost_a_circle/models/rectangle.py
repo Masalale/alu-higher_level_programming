7e5b620d53aa2e4ac66f62713dd9edea884e7321
Masalale <clarenchomba59@gmail.com>
2025-03-24 21:22:38 +0200

    Add Rectangle class for rectangle representation with validation

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+"""
+Rectangle module - implements Rectangle class that inherits from Base
+"""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Rectangle class for representing rectangles
+
+    Inherits from the Base class and adds width, height,
+    and position attributes with validation.
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle instance
+
+        Args:
+            width: Width of the rectangle
+            height: Height of the rectangle
+            x: X position of the rectangle
+            y: Y position of the rectangle
+            id: Identifier for this rectangle
+        """
+        # Call the parent class constructor with id
+        super().__init__(id)
+
+        # Set each attribute through properties for validation
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Get the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """Get the x coordinate of the rectangle"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Set the x coordinate of the rectangle"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Get the y coordinate of the rectangle"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Set the y coordinate of the rectangle"""
+        self.__y = value
