c2769cfed996818238e1418cf78d984db2a0af66
Masalale <clarenchomba59@gmail.com>
2025-03-24 21:17:51 +0200

    Add models package with base class for object ID management

---
+++ python-almost_a_circle/models/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""
+Models package for the Almost a Circle project.
+Contains geometric shape classes and utility modules.
+"""
---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Base module - foundation for all classes in the project
+"""
+
+
+class Base:
+    """Base class for managing object IDs
+    
+    This class handles ID assignment for all derived classes,
+    helping to avoid duplicate code and reduce bugs.
+    """
+    __nb_objects = 0
+    
+    def __init__(self, id=None):
+        """Set up a new instance with a unique ID
+        
+        Args:
+            id: Optional identifier value
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
