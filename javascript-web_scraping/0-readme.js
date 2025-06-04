8a682364bf0b4fe1eb50d6ef1349a1c59fe45f82
Masalale <clarenchomba59@gmail.com>
2025-06-04 21:37:33 +0200

    replace 'file' module with 'fs' for reading files

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const file = require('file');
+const fs = require('fs');
 const filePath = process.argv[2];
 
-file.readFile(filePath, 'utf-8', (err, data) => {
+fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
